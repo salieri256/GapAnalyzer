2b0b04c38c230dc51ba2a5e386ebc047ea8c2aca
さりえり <nakakou201250@gmail.com>
2022-09-05 19:19:04 +0900

    Add feature to convert `report.html` to DOM

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
+import { readFile } from 'fs/promises'
 import { createInterface } from 'readline'
+import { JSDOM } from 'jsdom'
 
 const main = async () => {
     const io = createInterface({
@@ -16,6 +18,18 @@ const main = async () => {
             return (await asyncio.next()).value
         }
     })()
+
+    const html = await (async () => {
+        try {
+            return await readFile(filePath, 'utf-8')
+        }
+        catch (e) {
+            console.log('File not found.')
+            process.exit(1)
+        }
+    })()
+
+    const dom = new JSDOM(html)
 
     io.close()
 }
